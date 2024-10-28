2702eb54e9ae6acc41a8dbea8e68e4878288ddb1
Sid-CS328 <siddhartharya.cs23@bmsce.ac.in>
2024-10-28 12:43:48 +0530

    Add files via upload

---
+++ SinglyLinkedList.c
@@ -0,0 +1,126 @@
+// Program to create a Singly Linked List
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+
+struct Node* createNode(int data){
+    struct Node *newNode = (struct Node*) malloc (sizeof(newNode));
+    newNode -> data = data;
+    newNode -> next = NULL;
+    return newNode;
+}
+
+
+
+void insertBeginning(struct Node **head, int data){
+    struct Node *newNode = createNode(data);
+    newNode -> next = *head;
+    *head = newNode;
+    printf("%d inserted at the Beginning \n", data);
+}
+
+
+
+void insertEnd(struct Node **head, int data){
+    struct Node *newNode = createNode(data);
+
+    if(*head == NULL){
+        *head = newNode;
+        return;
+    }
+
+    struct Node *temp = *head;
+
+    while(temp -> next != NULL){
+        temp = temp -> next;
+    }
+
+    temp -> next = newNode;
+
+    printf("%d inserted at the End \n", data);
+}
+
+
+
+
+void display(struct Node *head){
+    struct Node *temp = head;
+    printf("\n");
+    printf("Linked List: \n");
+    while (temp != NULL){
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
+    }
+
+    printf("NULL \n");
+}
+
+
+
+
+int main(){
+    struct Node *head = NULL;
+    struct Node *temp = head;
+
+    int choice, value;
+
+
+    do{
+        printf("\n Linked List Operations: \n");
+        printf("1. Insert at the Beginning \n");
+        printf("2. Insert at the End \n");
+        printf("3. Display the List \n");
+        printf("4. Exit \n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+
+        switch(choice){
+            case 1:
+                printf("Enter Value to insert:");
+                scanf("&d", &value);
+                insertBeginning(&head, value);
+                break;
+
+            case 2:
+                printf("Enter Value to insert:");
+                scanf("&d", &value);
+                insertEnd(&head, value);
+                break;
+
+            case 3:
+                display(head);
+                break;
+
+            case 4:
+                printf("Exiting \n");
+                break;
+
+            default:
+                printf("Invalid choice! \n");
+        }
+    } while(choice != 4);
+
+
+    while(head != NULL){
+        temp = head;
+        head = head -> next;
+        free(temp);
+    }
+
+
+    return 0;
+}
+
+
+
+
