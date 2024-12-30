cc12a32c1942a9b2d416b7532c6bbd8117df6048
Sid-CS328 <siddhartharya.cs23@bmsce.ac.in>
2024-12-30 09:12:27 +0530

    Update SinglyLinkedList.c

--- Week 4/SinglyLinkedList.c
+++ Week 4/SinglyLinkedList.c
@@ -1,130 +1,178 @@
 // Program to create a Singly Linked List
 
-
 #include <stdio.h>
 #include <stdlib.h>
 
 
-struct Node{
+struct Node {
     int data;
     struct Node *next;
 };
 
 
-struct Node* createNode(int data){
-    struct Node *newNode = (struct Node*) malloc (sizeof(newNode));
-    newNode -> data = data;
-    newNode -> next = NULL;
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
     return newNode;
 }
 
 
+struct Node* createLinkedList() {
+    int data;
+    struct Node *head = NULL, *temp = NULL;
+    char choice;
 
-void insertBeginning(struct Node **head, int data){
-    struct Node *newNode = createNode(data);
-    newNode -> next = *head;
-    *head = newNode;
-    printf("%d inserted at the Beginning \n", data);
+    do {
+        printf("Enter data for new node: ");
+        scanf("%d", &data);
+
+        struct Node* newNode = createNode(data);
+
+        
+        if (head == NULL) {
+            head = newNode;
+        } else {
+            temp->next = newNode;
+        }
+        temp = newNode; 
+
+        printf("Do you want to add another node (y/n)? ");
+        scanf(" %c", &choice); 
+    } while (choice == 'y' || choice == 'Y');
+
+    return head;
 }
 
 
+struct Node* insertAtFirst(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = head;
+    head = newNode;
+    return head;
+}
 
-void insertEnd(struct Node **head, int data){
-    struct Node *newNode = createNode(data);
 
-    if(*head == NULL){
-        *head = newNode;
+struct Node* insertAtEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    if (head == NULL) {
+        return newNode;
+    }
+
+    struct Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+
+    return head;
+}
+
+
+struct Node* insertAtPosition(struct Node* head, int data, int position) {
+    if (position <= 0) {
+        printf("Invalid position! Insertion failed.\n");
+        return head;
+    }
+
+    struct Node* newNode = createNode(data);
+
+
+    if (position == 1) {
+        newNode->next = head;
+        head = newNode;
+        return head;
+    }
+
+    struct Node* temp = head;
+    int count = 1;
+
+   
+    while (temp != NULL && count < position - 1) {
+        temp = temp->next;
+        count++;
+    }
+
+ 
+    if (temp == NULL) {
+        printf("Position is greater than the length of the list.\n");
+        free(newNode);
+        return head;
+    }
+
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+    return head;
+}
+
+
+void displayLinkedList(struct Node* head) {
+    if (head == NULL) {
+        printf("The list is empty.\n");
         return;
     }
 
-    struct Node *temp = *head;
-
-    while(temp -> next != NULL){
-        temp = temp -> next;
+    struct Node* temp = head;
+    printf("Linked List: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
     }
-
-    temp -> next = newNode;
-
-    printf("%d inserted at the End \n", data);
+    printf("NULL\n");
 }
 
+int main() {
+    struct Node* head = NULL;
+    int choice, data, position;
 
-
-
-void display(struct Node *head){
-    struct Node *temp = head;
-    printf("\n");
-    printf("Linked List: \n");
-    while (temp != NULL){
-        printf("%d -> ", temp -> data);
-        temp = temp -> next;
-    }
-
-    printf("NULL \n");
-}
-
-
-
-
-int main(){
-    struct Node *head = NULL;
-    struct Node *temp = head;
-
-    int choice, value_data;
-
-
-    do{
-        printf("\n Linked List Operations: \n");
-        printf("1. Insert at the Beginning \n");
-        printf("2. Insert at the End \n");
-        printf("3. Display the List \n");
-        printf("4. Exit \n");
-
+    while (1) {
+        printf("\nLinked List Operations:\n");
+        printf("1. Create a linked list\n");
+        printf("2. Insert at first position\n");
+        printf("3. Insert at any position\n");
+        printf("4. Insert at end position\n");
+        printf("5. Display linked list\n");
+        printf("6. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-
-        switch(choice){
+        switch (choice) {
             case 1:
-                //printf("%d \n", value_data);
-                printf("Enter Value to insert:");
-                scanf("&d", &value_data);
-
-                //printf("%d \n", value_data);
-
-                insertBeginning(&head, value_data);
+                head = createLinkedList();
+                printf("Linked list created successfully!\n");
                 break;
-
             case 2:
-                printf("Enter Value to insert:");
-                scanf("&d", &value_data);
-                insertEnd(&head, value_data);
+                printf("Enter data to insert at the first position: ");
+                scanf("%d", &data);
+                head = insertAtFirst(head, data);
+                printf("Node inserted at the first position.\n");
                 break;
-
             case 3:
-                display(head);
+                printf("Enter data to insert: ");
+                scanf("%d", &data);
+                printf("Enter position to insert at: ");
+                scanf("%d", &position);
+                head = insertAtPosition(head, data, position);
+                printf("Node inserted at position %d.\n", position);
                 break;
-
             case 4:
-                printf("Exiting \n");
+                printf("Enter data to insert at the end: ");
+                scanf("%d", &data);
+                head = insertAtEnd(head, data);
+                printf("Node inserted at the end of the list.\n");
                 break;
-
+            case 5:
+                displayLinkedList(head);
+                break;
+            case 6:
+                printf("Exiting the program.\n");
+                exit(0);
             default:
-                printf("Invalid choice! \n");
+                printf("Invalid choice! Please try again.\n");
         }
-    } while(choice != 4);
-
-
-    while(head != NULL){
-        temp = head;
-        head = head -> next;
-        free(temp);
     }
-
 
     return 0;
 }
-
-
-
-
